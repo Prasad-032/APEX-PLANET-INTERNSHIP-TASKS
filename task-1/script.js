6d6780f883a619396205ac5304a9d695d40f8fdf
Prasad-032 <166014183+Prasad-032@users.noreply.github.com>
2025-08-30 13:12:28 +0530

    Add files via upload

---
+++ task-1/script.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const submitButton = document.querySelector("#connect button");
+
+    if (submitButton) {
+        submitButton.addEventListener("click", () => {
+            alert("Thanks for your support!");
+        });
+    }
+});
